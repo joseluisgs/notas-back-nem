2c7d788c8c87dd273a59277d036f9c8d7ef3d9d5
joseluisgs <joseluisgonsan@gmail.com>
2020-06-05 11:35:10 +0200

    Rutas Notas

--- src/routes/notas.js
+++ src/routes/notas.js
@@ -16,9 +16,20 @@ const router = express.Router();
 // const { auth } = require('../middlewares/auth'); // es equivalente a poner const auth = requiere ('...').auth;
 // const { role } = require('../middlewares/auth');
 
-router.get('/', (req, res) => {
-  res.status(200).send('Hola notas');
-});
+// GET Listar todos los elementos, podemos hacerlo todos. Si no se pone role es que esta implícito role(['user'])
+router.get('/', notasController.notas);
+
+// GET Obtiene un elemento por por ID, podemos hacerlo todos
+router.get('/:id', notasController.notaById);
+
+// POST Añadir Elemento. Solo autenticados y del nivel admin, por eso no se pone nada (es por defecto)
+router.post('/', notasController.addNota);
+
+// PUT Modifica un elemento por ID. Solo autenticados y del nivel admin, por eso no se pone nada (es por defecto)
+router.put('/:id', notaController.editNotaById);
+
+// DELETE Elimina un elemento por ID. Solo autenticados y del nivel admin podrán, por eso no se pone nada (es por defecto)
+router.delete('/:id', notasController.deleteNotaById);
 
 // Exprotamos el módulo
 export default router;
