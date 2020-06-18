86dca98d17c53f9db10d8756078c666f94b32c2f
joseluisgs <joseluisgonsan@gmail.com>
2020-06-18 14:32:05 +0200

    Confi Server

--- src/config.js
+++ src/config.js
@@ -36,7 +36,7 @@ export default (app) => {
     {
       createParentPath: true,
       limits: { fileSize: env.FILE_SIZE * 1024 * 1024 }, // en MB
-      useTempFiles: false, // Uso de ficheros temporales
+      useTempFiles: false, // Uso de ficheros temporales. Debes ponerlo a false si vas a publicar en Heroku (tema de ficheros temporales en dicha plataforma)
       tempFileDir: '/tmp/', // Usamos un directorio y ficheros temporal y no memoria para el proceso de subida.
       preserveExtension: true, // dejamos la extensión por defecto
       debug: env.NODE_ENV !== 'test' ? env.DEBUG : false, // Modo de depuración (no saco los mensajes en test)
